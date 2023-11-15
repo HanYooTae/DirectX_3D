9f9877bb6cc5aaea484d119f85a0273e88f804f0
rlatmdgh35 <41534351+HanYooTae@users.noreply.github.com>
2023-11-15 19:53:25 +0900

    shader

--- D3D/UnitTest/IndexBufferDemo.cpp
+++ D3D/UnitTest/IndexBufferDemo.cpp
@@ -1,18 +1,16 @@
 #include "stdafx.h"
-#include "WorldDemo.h"
+#include "IndexBufferDemo.h"
 
-void WorldDemo::Initialize()
+void IndexBufferDemo::Initialize()
 {
 	shader = new Shader(L"04_World.fxo");
 
-	// Polygon1
+	//Create Vertex Buffer
 	{
 		vertices[0].Position = Vector3(-0.5f, -0.5f, +0.0f);
 		vertices[1].Position = Vector3(-0.5f, +0.5f, +0.0f);
 		vertices[2].Position = Vector3(+0.5f, -0.5f, +0.0f);
-		vertices[3].Position = Vector3(+0.5f, -0.5f, +0.0f);
-		vertices[4].Position = Vector3(-0.5f, +0.5f, +0.0f);
-		vertices[5].Position = Vector3(+0.5f, +0.5f, +0.0f);
+		vertices[3].Position = Vector3(+0.5f, +0.5f, +0.0f);
 
 		D3D11_BUFFER_DESC desc;
 		ZeroMemory(&desc, sizeof(D3D11_BUFFER_DESC));
@@ -22,36 +20,56 @@ void WorldDemo::Initialize()
 		D3D11_SUBRESOURCE_DATA subResource = { 0 };
 		subResource.pSysMem = vertices;
 
-		//HRESULT result = D3D::GetDevice()->CreateBuffer(&desc, &subResource, &vertexBuffer);
-		//// S_OK가 아니라면 터짐
-		//assert(SUCCEEDED(result));
 		Check(D3D::GetDevice()->CreateBuffer(&desc, &subResource, &vertexBuffer));
+	}
+
+	//Create Index Buffer
+	{
+		indices[0] = 0;
+		indices[1] = 1;
+		indices[2] = 2;
+		indices[3] = 2;
+		indices[4] = 1;
+		indices[5] = 3;
+
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(D3D11_BUFFER_DESC));
+		desc.ByteWidth = sizeof(UINT) * ARRAYSIZE(indices);
+		desc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+
+		D3D11_SUBRESOURCE_DATA subResource = { 0 };
+		subResource.pSysMem = indices;
+
+		Check(D3D::GetDevice()->CreateBuffer(&desc, &subResource, &indexBuffer));
 	}
 
 	D3DXMatrixIdentity(&world);
 }
 
-void WorldDemo::Destroy()
+void IndexBufferDemo::Destroy()
 {
+	SafeRelease(indexBuffer);
 	SafeRelease(vertexBuffer);
 	SafeDelete(shader);
 }
 
-void WorldDemo::Update()
+void IndexBufferDemo::Update()
 {
-	// x : world._41
-	// y : world._42
-	// z : world._43
+	// x 위치 : world._41
+	// y 위치 : world._42
+	// z 위치 : world._43
+
 #ifdef MatrixMember
 	if (Keyboard::Get()->Press('D'))
 		world._41 += 2.f * Time::Delta();
 	else if (Keyboard::Get()->Press('A'))
 		world._41 -= 2.f * Time::Delta();
-	else if (Keyboard::Get()->Press('W'))
+
+	if (Keyboard::Get()->Press('W'))
 		world._42 += 2.f * Time::Delta();
 	else if (Keyboard::Get()->Press('S'))
 		world._42 -= 2.f * Time::Delta();
-#endif // MatrixMember
+#endif
 
 	static Vector3 position = Vector3(0, 0, 0);
 	static Vector3 scale = Vector3(1, 1, 1);
@@ -62,19 +80,20 @@ void WorldDemo::Update()
 			scale.x += 2.f * Time::Delta();
 		else if (Keyboard::Get()->Press('A'))
 			scale.x -= 2.f * Time::Delta();
-		else if (Keyboard::Get()->Press('W'))
+
+		if (Keyboard::Get()->Press('W'))
 			scale.y += 2.f * Time::Delta();
 		else if (Keyboard::Get()->Press('S'))
 			scale.y -= 2.f * Time::Delta();
 	}
-
 	else
 	{
 		if (Keyboard::Get()->Press('D'))
 			position.x += 2.f * Time::Delta();
 		else if (Keyboard::Get()->Press('A'))
 			position.x -= 2.f * Time::Delta();
-		else if (Keyboard::Get()->Press('W'))
+
+		if (Keyboard::Get()->Press('W'))
 			position.y += 2.f * Time::Delta();
 		else if (Keyboard::Get()->Press('S'))
 			position.y -= 2.f * Time::Delta();
@@ -90,15 +109,16 @@ void WorldDemo::Update()
 	shader->AsMatrix("Projection")->SetMatrix(Context::Get()->Projection());
 }
 
-void WorldDemo::Render()
+void IndexBufferDemo::Render()
 {
 	UINT stride = sizeof(Vertex);
 	UINT offset = 0;
 	D3D::GetDC()->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+	D3D::GetDC()->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
 	D3D::GetDC()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	static bool bWireFrame;
 	ImGui::Checkbox("WireFrame", &bWireFrame);
 
-	shader->Draw(0, bWireFrame ? 1 : 0, 6);
+	shader->DrawIndexed(0, bWireFrame ? 1 : 0, 6);
 }
